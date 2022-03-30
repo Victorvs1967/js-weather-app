771931786c12aac481a9034dc3ceaf869f45ced2
Victor <victorsmirnov@me.com>
2022-03-30 11:32:13 +0300

    add data file

--- js/main.js
+++ js/main.js
@@ -3,24 +3,28 @@ const form = document.querySelector('.top-banner form'),
       msg = document.querySelector('.top-banner .msg'),
       list = document.querySelector('.ajax-section .cities');
 
-const apiKey = 'e1f7d88c2aaf836677ef89091aeade4a';
+//  apiKey in data.json
 
-form.addEventListener('submit', event => {
-  event.preventDefault();
-  const listItems = list.querySelectorAll('.ajax-section .city');
-  const inputVal = input.value;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+fetch('./data.json')
+  .then(data => data.json())
+  .then(result => result.apiKey)
+  .then(apiKey => {
+    form.addEventListener('submit', event => {
+      event.preventDefault();
+      const listItems = list.querySelectorAll('.ajax-section .city');
+      const inputVal = input.value;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
 
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
 
-      const { main, name, sys, weather } = data;
-      const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+          const { main, name, sys, weather } = data;
+          const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
 
-      const li = document.createElement('li');
-      li.classList.add('city');
-      const markup = `
+          const li = document.createElement('li');
+          li.classList.add('city');
+          const markup = `
         <h2 class="city-name" data-name="${name},${sys.country}">
           <span>${name}</span>
           <sup>${sys.country}</sup>
@@ -31,12 +35,13 @@ form.addEventListener('submit', event => {
           <figcaption>${weather[0]["description"]}</figcaption>
         </figure>
       `;
-      li.innerHTML = markup;
-      list.appendChild(li);
-    })
-    .catch(() => msg.textContent = "Pleas search a valid city 😩");
+          li.innerHTML = markup;
+          list.appendChild(li);
+        })
+        .catch(() => msg.textContent = "Pleas search a valid city 😩");
 
-    msg.textContent = '';
-    form.reset();
-    input.focus();
+      msg.textContent = '';
+      form.reset();
+      input.focus();
+    });
 });
