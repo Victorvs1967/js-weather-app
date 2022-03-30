ea6be83a46d96cef1398b86be6d7b58572626ca2
Victor <victorsmirnov@me.com>
2022-03-30 11:43:50 +0300

    refactoring js file

--- js/main.js
+++ js/main.js
@@ -3,45 +3,45 @@ const form = document.querySelector('.top-banner form'),
       msg = document.querySelector('.top-banner .msg'),
       list = document.querySelector('.ajax-section .cities');
 
-//  apiKey in data.json
+const viewWeather = apiKey => {
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+    const listItems = list.querySelectorAll('.ajax-section .city');
+    const inputVal = input.value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
 
-fetch('./data.json')
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+
+        const { main, name, sys, weather } = data;
+        const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+
+        const li = document.createElement('li');
+        li.classList.add('city');
+        const markup = `
+            <h2 class="city-name" data-name="${name},${sys.country}">
+              <span>${name}</span>
+              <sup>${sys.country}</sup>
+            </h2>
+            <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+            <figure>
+              <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
+              <figcaption>${weather[0]["description"]}</figcaption>
+            </figure>
+          `;
+        li.innerHTML = markup;
+        list.appendChild(li);
+      })
+      .catch(() => msg.textContent = "Pleas search a valid city 😩");
+
+    msg.textContent = '';
+    form.reset();
+    input.focus();
+  });
+};
+
+fetch('./data.json') // apiKey from data.json
   .then(data => data.json())
   .then(result => result.apiKey)
-  .then(apiKey => {
-    form.addEventListener('submit', event => {
-      event.preventDefault();
-      const listItems = list.querySelectorAll('.ajax-section .city');
-      const inputVal = input.value;
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
-
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-
-          const { main, name, sys, weather } = data;
-          const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
-
-          const li = document.createElement('li');
-          li.classList.add('city');
-          const markup = `
-        <h2 class="city-name" data-name="${name},${sys.country}">
-          <span>${name}</span>
-          <sup>${sys.country}</sup>
-        </h2>
-        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
-        <figure>
-          <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
-          <figcaption>${weather[0]["description"]}</figcaption>
-        </figure>
-      `;
-          li.innerHTML = markup;
-          list.appendChild(li);
-        })
-        .catch(() => msg.textContent = "Pleas search a valid city 😩");
-
-      msg.textContent = '';
-      form.reset();
-      input.focus();
-    });
-});
+  .then(apiKey => viewWeather(apiKey));
